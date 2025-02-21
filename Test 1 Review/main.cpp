ce40c701c01870c7c4c5940a376d8a8d06f08978
Jass <136769749+frontendjass@users.noreply.github.com>
2025-02-21 16:41:01 -0600

    Initial Commit

---
+++ Test 1 Review/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test 1 Review
+//
+//  Created by Juan Sosa on 2/21/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
